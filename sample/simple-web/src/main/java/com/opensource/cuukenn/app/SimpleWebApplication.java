ab5d380adadd920f6263d6ccaf21b04def451805
changgg <891842749@qq.com>
2022-10-10 10:19:05 +0800

    feat: simple SSE demo

---
+++ sample/simple-web/src/main/java/com/opensource/cuukenn/app/SimpleWebApplication.java
@@ -0,0 +1,14 @@
+package com.opensource.cuukenn.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author changgg
+ */
+@SpringBootApplication
+public class SimpleWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebApplication.class, args);
+    }
+}
