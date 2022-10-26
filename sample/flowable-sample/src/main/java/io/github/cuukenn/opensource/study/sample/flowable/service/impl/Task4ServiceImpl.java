15a287b058be90b8ff49f7366ff8a6380a7fa516
changgg <891842749@qq.com>
2022-10-26 19:32:17 +0800

    feat: add event & para gateway test for flowable
    结论：没实现期望结果
    事件网关+并行网关,事件触发一次后，其他事件分支不再触发
    无法做到依靠事件的任务全部完成后再汇聚到并行网关执行后续的任务完成逻辑

---
+++ sample/flowable-sample/src/main/java/io/github/cuukenn/opensource/study/sample/flowable/service/impl/Task4ServiceImpl.java
@@ -0,0 +1,19 @@
+package io.github.cuukenn.opensource.study.sample.flowable.service.impl;
+
+import io.github.cuukenn.opensource.study.sample.flowable.service.ITaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author changgg
+ */
+@Service
+public class Task4ServiceImpl implements ITaskService {
+    private static final Logger log = LoggerFactory.getLogger(Task4ServiceImpl.class);
+
+    @Override
+    public void invoke(String param) {
+        log.info("[work][invoke 4 work]");
+    }
+}
